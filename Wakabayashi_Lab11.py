db2498e78df3fbbb855ac13aa7314fa0a9d473b4
kyouko5149 <wakabayashi.kyouko04@gmail.com>
2024-11-14 14:45:01 +0800

    Update Wakabayashi_Lab11_Grades.py

--- Wakabayashi_Lab11.py
+++ Wakabayashi_Lab11.py
@@ -1,23 +1,29 @@
 
 grade_list = []
-passed = []
+passed_subject = []
+
 
 while True:
     grade = input("Enter your grade (Type 'Done/done' to exit): ")
+    
     if grade.lower() == 'done':
         break
+    if not grade.isdigit() or int(grade) <= 39 or int(grade) >= 101:
+        print("Invalid input. Please enter grade between 40 and 100 next time.")
     else:
         grade = int(grade)
         grade_list.append(grade)
         if grade >= 75:
-            passed.append(grade)
-            print("Your grade is", grade)
+            passed_subject.append(grade)
+        print("Your grade is", grade)
+        
 if grade_list:
-    grade_average = sum(grade_list) / len(grade_list)
-    passing_percentage = len(passed) / len(grade_list) * 100
-    passed_percentage = int(passing_percentage)
-    print(f"Your Average grade is {grade_average:.2f}")
-    print(f"Your Passing percentage is {passing_percentage:.2f}%")
-    print(f"Your Grades: {grade_list}")
+    grade_average = sum(grade_list) / len(grade_list) if grade_list else 0
+    passing_percentage = (len(passed_subject) / len(grade_list)) * 100
+    
+    print(f"The Average grade is {grade_average:.2f}")
+    print(f"The Passing percentage is {passing_percentage:.2f}%")
+    print(f"Number of students who passed: {len(passed_subject)}")
+    print(f"The Grades: {grade_list}")
 else:
     print("There have been no grade entered.")
